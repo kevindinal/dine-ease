bde92af9fd54ae62b43edf0a271469b5958e2656
Pesandu Rashodya <pesandurashodya@gmail.com>
2025-02-21 04:52:39 +0530

    Did some changes to firebase file

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
+import { doc, getDoc, getDocs, getFirestore } from "firebase/firestore";
+import { collection, query, where } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,4 +23,71 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 const db = getFirestore(app);
-export { db };
+
+export const getRestaurantDetails = async (restaurant_Id: string) => {
+  try {
+    const docRef = doc(db, "restaurants", restaurant_Id);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      return {
+        id: docSnap.id,
+        ...docSnap.data(),
+      }
+    } else {
+      console.log("No such document!");
+      return null;
+    }
+
+  } catch (error) {
+    console.error("Error getting restaurant: ", error);
+    return null;
+    
+  }
+};
+export const getMealsByRestaurant = async (restaurant_Id: string) => {
+  try {
+    //Query the meals collection for meals with the restaurant
+    const mealsQuery = query(collection(db, "meals"), where("restaurant_id", "==", restaurant_Id));
+    const mealsSnapshot = await getDocs(mealsQuery);
+    
+    //Concerting the snapshot to an array of objects
+    return mealsSnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+  } catch (error) {
+    console.error("Error getting meals: ", error);
+    return [];
+  }
+};
+
+export const getMealsByCategoryInRestaurant = async (restaurant_Id: string, category_Id: string) => {
+  try {
+    const mealsQuery = query(collection(db, "meals"), where("restaurant_id", "==", restaurant_Id), where("category", "==", category_Id));
+
+    const mealsSnapshot = await getDocs(mealsQuery);
+
+    return mealsSnapshot.docs.map((doc) => ({
+      id: doc.id, 
+      ...doc.data(),
+    }));
+  } catch (error) {
+    console.error("Error getting meals: ", error);
+    return [];
+  }
+};
+
+export const getCategoriesByRestaurant = async (restaurant_Id: string) => {
+  try {
+    const categoryQuery = query(collection(db, "categories"), where("restaurant_id", "==", restaurant_Id));
+    const categorySnapshot = await getDocs(categoryQuery);
+
+    return categorySnapshot.docs.map((doc) => ({ 
+      id: doc.id,
+      ...doc.data
+    }));
+  } catch (error) {
+    console.error("Error getting categories: ", error);
+    return [];
+  }
+}
