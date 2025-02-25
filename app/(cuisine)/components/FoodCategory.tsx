6be551d31ef75ac1926750a3c165cc3e7461c5a0
Pesandu Rashodya <pesandurashodya@gmail.com>
2025-02-25 09:52:16 +0530

    Changed the style of Food Category section

--- app/(cuisine)/components/FoodCategory.tsx
+++ app/(cuisine)/components/FoodCategory.tsx
@@ -9,13 +9,13 @@ interface FoodCategoryProps {
 const FoodCategory: React.FC<FoodCategoryProps> = ({ imageSrc, foodType }) => {
   return (
     <div className='flex flex-col items-center'>
-      <div className='overflow-hidden shadow-md rounded-full'>
+      <div className='w-24 h-24 overflow-hidden shadow-md rounded-full'>
         <Image
           src={imageSrc}
           alt={foodType}
-          width={100}
-          height={100}
-          className='w-full h-full object-cover rounded-full'
+          width={96}   // 96px width (adjust for size)
+          height={96}  // 96px height (adjust for size)
+          className='object-cover w-full h-full'
         />
       </div>
       <p className='mt-2 text-sm font-medium text-gray-700'>{foodType}</p>
