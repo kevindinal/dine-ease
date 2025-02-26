053344f9405c4264bea7db8ca7ace15807c23ad0
kevindinal <kevindinal2005@gmail.com>
2025-02-26 11:28:09 +0530

    Dataset updated

--- data/restaurrants.ts
+++ data/restaurrants.ts
@@ -48,5 +48,38 @@ export type Restaurant = {
       ],
       times: ["12:00 PM", "12:15 PM", "12:30 PM"],
     },
+
+    {
+      id: 2,
+      name: "The Lagoon - Cinnamon Grand Colombo",
+      image: "https://www.cinnamonhotels.com/themes/cinnamon/images/cinnamon_grand_lagoon.jpg",
+      bannerImage: "https://media.timeout.com/images/105239239/750/422/image.jpg",
+      rating: 4,
+      reviews: 95,
+      cuisine: ["Seafood", "Sri Lankan", "Asian"],
+      category: "Seafood, Sri Lankan - $$$$",
+      priceRange: "$$$$",
+      description: "Experience the Best Seafood Dining in Colombo",
+      about: "The Lagoon at Cinnamon Grand Colombo is renowned for its fresh seafood, offering an exquisite selection of dishes inspired by Sri Lankan and Asian flavors. The ambiance is perfect for seafood lovers looking for a premium dining experience.",
+      address: "77 Galle Road, Colombo 03",
+      location: "Cinnamon Grand Colombo, Sri Lanka",
+      featuredMenu: [
+        { name: "Chili Crab", price: "$45", image: "https://www.marionskitchen.com/wp-content/uploads/2023/04/Singapore-Chilli-Crab2.jpg" },
+        { name: "Garlic Butter Prawns", price: "$38", image: "https://cafedelites.com/wp-content/uploads/2018/07/Garlic-Butter-Shrimp-IMAGE-46.jpg" },
+        { name: "Grilled Lobster", price: "$60", image: "https://www.seriouseats.com/thmb/v3bwZjTg3sL0JzzIzcd5GGpqlwU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/20230720-GrilledLobster-VickyWasik-8-d52ff009f12b467d8015648d71fa4168.jpg" },
+        { name: "Sri Lankan Crab Curry", price: "$50", image: "https://img.delicious.com.au/L0r1xyRs/del/2017/10/sri-lankan-crab-curry-66416-2.jpg" },
+        { name: "Tandoori Prawns", price: "$35", image: "https://www.indianhealthyrecipes.com/wp-content/uploads/2022/03/tandoori-prawns-recipe.jpg" },
+        { name: "Seafood Paella", price: "$42", image: "https://www.simplyrecipes.com/thmb/RlMwlGFPkpTETq4yyYeLtkGnblY=/750x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Seafood-Paella-LEAD-09-21aaf7c27fd64c5796c6d19ac7ff4c30.jpg" },
+      ],
+      photos: [
+        "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/00/c7/59/lagoon-seafood-restaurant.jpg?w=1200&h=-1&s=1",
+        "https://www.cinnamonhotels.com/themes/cinnamon/images/cinnamon_grand_lagoon_gallery_2.jpg",
+        "https://media-cdn.tripadvisor.com/media/photo-s/11/5c/96/0e/the-lagoon.jpg",
+        "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/6c/3d/d9/the-lagoon.jpg?w=1200&h=-1&s=1",
+      ],
+      times: ["11:30 AM", "12:00 PM", "12:30 PM", "01:00 PM"],
+    },
+    
+    
   ];
   
