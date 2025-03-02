d2b930d2c34c6d259a0e0ebcebf2c702ce0667c9
Pesandu Rashodya <pesandurashodya@gmail.com>
2025-03-02 06:20:23 +0530

    Updated meal main page

--- app/(cuisine)/cuisine-main-page/page.tsx
+++ app/(cuisine)/cuisine-main-page/page.tsx
@@ -6,13 +6,14 @@ import FoodCategory from "../components/FoodCategory";
 import { Readex_Pro } from "next/font/google";
 import FoodCard from "../components/FoodCard";
 import FloatingButtons from "../components/FloatingButtons";
-import { recommendedForYou, todaysSpecials, chefsSpecials } from "../data/data";
+import { recommendedForYou } from "../data/data";
 
 import { useRestaurant } from "../hooks/useRestaurant";
 import { useCategories } from "../hooks/useCategories";
-import {useMealsByCategory } from "../hooks/useMeals";
-
-const readexPro = Readex_Pro({ subsets: ["latin"], weight: ["400", "700"] });
+import { useChefsSpecials } from "../hooks/useChefsSpecials";
+import { useTodaysSpecials } from "../hooks/useTodaysSpecials";
+import { useMeals } from "../hooks/useMeals";
+import { Category } from "../types/category";
 
 interface MealPreOrderMainProps {
   hotelImage?: string;
@@ -23,44 +24,33 @@ export default function MealPreOrderMain({ hotelImage }: MealPreOrderMainProps) 
   const restaurantId = "restaurant_1";
   const categoryId = "category_1";
 
-  const { restaurant, loading: restaurantLoading, error: restaurantError} = useRestaurant(restaurantId || "");
-  const { categories, loading: categoriesLoading, error: categoriesError} = useCategories(restaurantId);
-  const { meals: allMeals, loading: mealsLoading, error: mealsError } = useMealsByCategory(restaurantId, categoryId);
+  const { restaurant, loading: restaurantLoading, error: restaurantError } = useRestaurant(restaurantId || "");
+  const { categories, loading: categoriesLoading, error: categoriesError } = useCategories(restaurantId);
+  const { meals: todaysSpecials, loading: todaysLoading, error: todaysSpecialsError } = useTodaysSpecials(restaurantId);
+  const { meals: chefsSpecials, loading: chefsLoading, error: chefsError } = useChefsSpecials(restaurantId);
 
   const { preOrderCount, addItemToPreOrder, clearPreOrder } = usePreOrder();
 
   const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
 
+  const { meals, loading: mealsLoading, error: mealsError } = useMeals(restaurantId, selectedCategory as string);
+
   const categoriesRef = useRef<HTMLDivElement>(null);
 
-  const { meals: categoryMeals, loading: categoryMealsLoading } = useMealsByCategory(restaurantId, selectedCategory || "");
-
-  const allDisplayedCuisines = selectedCategory ? categoryMeals : allMeals;
-
-  const handleCategoryClick = (category: string | null) => {
-    setSelectedCategory(category);
+  const handleCategoryClick = (category: Category | string | null) => {
+    if (typeof category === 'string') {
+      setSelectedCategory(category);
+    } else if (category) {
+      setSelectedCategory(category.id);
+    } else {
+      setSelectedCategory(null);
+    }
   };
-
-  const getRecommendedMeals = () => {
-    return allMeals.filter((meal, index) => index < 5);
-  };
-
-  const getChefSpecials = () => {
-    return allMeals.filter((meal, index) => index >= 5 && index < 10);
-  }
-
-  const getTodaysSpecials = () => {
-    return allMeals.filter((meal, index) => index > 5);
-  }
-
-  const filteredCuisines = selectedCategory
-    ? allDisplayedCuisines.filter(() =>  selectedCategory)
-    : null;
 
   const renderFoodCard = (cuisine: any) => (
     <div key={cuisine.id}>
       <div className="h-full mb-8">
-        <FoodCard {...cuisine} onAddToPreOrder={addItemToPreOrder} />
+        <FoodCard {...cuisine} restaurantId = {restaurantId} categoryId = { categoryId }onAddToPreOrder={addItemToPreOrder} />
       </div>
     </div>
   );
@@ -71,13 +61,13 @@ export default function MealPreOrderMain({ hotelImage }: MealPreOrderMainProps) 
   if (!restaurant) return <div>Restaurant not found</div>;
 
   return (
-    <div className={`${readexPro.className} bg-fixed min-h-screen bg-[#F5F5F5]`}>
+    <div className={` bg-fixed min-h-screen bg-[#F5F5F5]`}>
 
       <section className="py-4 mx-4 md:mx-14 z-10 fixed slide-in-from-bottom-28 left-0 right-0 flex justify-center bottom-24">
         <FloatingButtons preOrderCount={preOrderCount} clearPreOrder={clearPreOrder} />
       </section>
 
-      <section className="relative bg-cover bg-center bg-no-repeat py-16 sm:py-32 px-4 md:px-14 text-white" style={{ backgroundImage: `url(${restaurant.mealPageHotelImage})`}}>
+      <section className="relative bg-cover bg-center bg-no-repeat py-16 sm:py-32 px-4 md:px-14 text-white" style={{ backgroundImage: `url(${restaurant.mealPageHotelImage})` }}>
         <div className="bg-[#121212] bg-opacity-70 backdrop-blur-sm p-6 sm:p-16">
           <h2 className="text-3xl sm:text-5xl font-semibold mb-4 sm:mb-6 tracking-wide">{restaurant.name}</h2>
           <p className="text-base sm:text-lg leading-relaxed font-light">
@@ -102,9 +92,9 @@ export default function MealPreOrderMain({ hotelImage }: MealPreOrderMainProps) 
             <div
               key={category.id}
               className="cursor-pointer flex-none snap-start flex flex-col items-center transition-all hover:scale-110"
-              onClick={() => handleCategoryClick(category.name)}
+              onClick={() => handleCategoryClick(category)}
             >
-              <FoodCategory imageSrc={category.image} foodType={category.name} />
+              <FoodCategory imageSrc={category.categoryImage} foodType={category.name} />
             </div>
           ))}
         </div>
@@ -114,11 +104,17 @@ export default function MealPreOrderMain({ hotelImage }: MealPreOrderMainProps) 
       {selectedCategory ? (
         <section className="py-6 sm:py-8 mx-4 md:mx-14">
           <h2 className="text-2xl sm:text-3xl font-semibold text-[#121212] mb-4 sm:mb-6 tracking-wide">
-            {selectedCategory}
+            {categories.find((category) => category.id === selectedCategory)?.name || 'Unknown Category'}
           </h2>
-           <div className="flex flex-wrap -mx-2 sm:-mx-4">
-            {filteredCuisines?.map(renderFoodCard)}
-          </div>
+          {mealsLoading ? (
+            <div>Loading...</div>
+          ) : meals && meals.length > 0 ? (
+            <div className="flex flex-wrap -mx-2 sm:-mx-4">
+              {meals.map(renderFoodCard)}
+            </div>
+          ) : (
+            <div>No meals found in this category.</div>
+          )}
         </section>
       ) : (
         <>
@@ -137,31 +133,31 @@ export default function MealPreOrderMain({ hotelImage }: MealPreOrderMainProps) 
             </div>
           </section>
 
-          <section className="py-6 sm:py-8 mx-4 md:mx-14 relative mt-8 sm:mt-12">
-            <div className="rounded-2xl shadow-sm">
-              <h2 className="text-2xl sm:text-3xl font-semibold text-[#121212] mb-4 sm:mb-6 ml-4 sm:ml-8 pt-4 sm:pt-6 tracking-wide">
-                Chef's Specials for you
-              </h2>
-              <div className="flex gap-4 sm:gap-6 overflow-x-auto p-4 sm:p-8 scrollbar-hide snap-x snap-mandatory">
-                {chefsSpecials.map((cuisine) => (
-                  <div key={cuisine.id} className="min-w-[250px] sm:min-w-[320px] lg:min-w-[calc(25%-1rem)] flex-none snap-start">
-                    {renderFoodCard(cuisine)}
-                  </div>
-                ))}
+          {chefsSpecials && chefsSpecials.length > 0 && (
+            <section className="py-6 sm:py-8 mx-4 md:mx-14 relative mt-8 sm:mt-12">
+              <div className="rounded-2xl shadow-sm">
+                <h2 className="text-2xl sm:text-3xl font-semibold text-[#121212] mb-4 sm:mb-6 ml-4 sm:ml-8 pt-4 sm:pt-6 tracking-wide">
+                  Chef's specials for you
+                </h2>
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4 sm:p-8">
+                  {chefsSpecials.map((cuisine) => renderFoodCard(cuisine))}
+                </div>
               </div>
-            </div>
-          </section>
+            </section>
+          )}
 
-          <section className="py-6 sm:py-8 mx-4 md:mx-14 relative mt-8 sm:mt-12">
-            <div className="rounded-2xl shadow-sm">
-              <h2 className="text-2xl sm:text-3xl font-semibold text-[#121212] mb-4 sm:mb-6 ml-4 sm:ml-8 pt-4 sm:pt-6 tracking-wide">
-                Today's specials for you
-              </h2>
-              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4 sm:p-8">
-                {todaysSpecials.map((cuisine) => renderFoodCard(cuisine))}
+          {todaysSpecials && todaysSpecials.length > 0 && (
+            <section className="py-6 sm:py-8 mx-4 md:mx-14 relative mt-8 sm:mt-12">
+              <div className="rounded-2xl shadow-sm">
+                <h2 className="text-2xl sm:text-3xl font-semibold text-[#121212] mb-4 sm:mb-6 ml-4 sm:ml-8 pt-4 sm:pt-6 tracking-wide">
+                  Today's specials for you
+                </h2>
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4 sm:p-8">
+                  {todaysSpecials.map((cuisine) => renderFoodCard(cuisine))}
+                </div>
               </div>
-            </div>
-          </section>
+            </section>
+          )}
         </>
       )}
     </div>
