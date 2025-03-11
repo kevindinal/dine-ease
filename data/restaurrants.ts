918dc72645ab6e95a71906342c4b386cdf6fae79
yasanduyaka2005 <yasanduyaka2005@gmail.com>
2025-03-11 10:58:42 +0530

    connecting the route restaurant -> table-rservation

--- data/restaurrants.ts
+++ data/restaurrants.ts
@@ -20,7 +20,7 @@ export type Restaurant = {
   export const restaurants: Restaurant[] = [
     {
       id: 1,
-      name: "FLOW - Hilton Colombo Residences",
+      name: "Hilton Colombo Residences",
       image: "https://hiltoncolombo1.com/uploads/poster/6881610003007Cover1.jpg",
       bannerImage: "https://resizer.otstatic.com/v2/photos/wide-xlarge/2/41686450.jpg",
       rating: 5,
@@ -29,7 +29,7 @@ export type Restaurant = {
       category: "Global, International - $$$",
       priceRange: "$$$",
       description: "Experience Modern Asian Fusion Dining",
-      about: "FLOW Restaurant at Hilton Colombo Residences offers an exceptional dining experience, featuring a diverse menu that spans Sri Lankan, Indian, Chinese, Japanese, and Western cuisines. Our casual elegant atmosphere provides the perfect setting for both intimate dinners and business meetings",
+      about: "Restaurant at Hilton Colombo Residences offers an exceptional dining experience, featuring a diverse menu that spans Sri Lankan, Indian, Chinese, Japanese, and Western cuisines. Our casual elegant atmosphere provides the perfect setting for both intimate dinners and business meetings",
       address: "279/6 Hospital Road",
       location: "Hilton Colombo, Sri Lanka",
       featuredMenu: [
