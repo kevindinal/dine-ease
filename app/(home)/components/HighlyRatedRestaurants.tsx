5bddb599630d284c4eafd6ac7df2bac4075184a2
imsarie02 <151487358+imsarie02@users.noreply.github.com>
2025-03-17 15:24:39 +0530

    Made some changes to the design

--- app/(home)/components/HighlyRatedRestaurants.tsx
+++ app/(home)/components/HighlyRatedRestaurants.tsx
@@ -48,11 +48,11 @@ const restaurants = [
 
 const HighlyRatedRestaurants = () => {
   return (
-    <div className="container mx-auto px-4 py-16">
-      <h2 className="text-3xl font-bold text-accent-dark mb-8 text-center">
+    <div className="container mx-auto px-10 py-16">
+      <h2 className="text-3xl font-bold mb-8 text-center">
         Highly Rated Restaurants
       </h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
         {restaurants.map((restaurant) => (
           <div
             key={restaurant.id}
