a9bd58a5bea64d0d24ed401b896e0f5022ee0f95
Yasandu Imanjith <89386702+yasandu0505@users.noreply.github.com>
2025-03-23 01:39:35 +0530

    fixing errors in restaurants

--- app/(reservations)/table-reservation/[id]/page.tsx
+++ app/(reservations)/table-reservation/[id]/page.tsx
@@ -749,6 +749,7 @@ export default function TableDetailsPage() {
   const rating = table?.rating || calculatedRating
 
   return (
+    
     <div className="bg-white min-h-screen">
       {/* Header with Navigation */}
       <div className="bg-white sticky top-0 z-50 shadow-sm">
