52fc4bb7621140e1548acd35d40b9077e7e4ec7b
Yasandu Imanjith <89386702+yasandu0505@users.noreply.github.com>
2025-03-23 16:17:37 +0530

    updating navbar

--- components/header/Navbar.tsx
+++ components/header/Navbar.tsx
@@ -378,7 +378,7 @@ const Navbar = () => {
                 <DropdownMenuItem className="cursor-pointer transition-colors duration-200 hover:bg-[#FA4032]/10 group" 
                 onClick={handleGoMyProfile}>
                   <Settings className="mr-2 h-4 w-4 group-hover:rotate-90 transition-transform duration-300" />
-                  <span>My Profile</span>
+                  <span>My Activity</span>
                 </DropdownMenuItem>
                 <DropdownMenuItem
                   className="cursor-pointer transition-colors duration-200 hover:bg-[#FA4032]/10 group"
@@ -551,9 +551,10 @@ const Navbar = () => {
                     <Button
                       variant="ghost"
                       className="w-full justify-start text-white hover:bg-[#FA4032]/50 transition-colors duration-200 group"
+                      onClick={handleGoMyProfile}
                     >
                       <Settings className="mr-2 h-4 w-4 group-hover:rotate-90 transition-transform duration-300" />
-                      Profile Settings
+                      My Activity
                     </Button>
                   </motion.div>
 
